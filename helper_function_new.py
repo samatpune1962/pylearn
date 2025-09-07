b6a6058f7bf6d2cd675cd3179b659249abc1f7ae
rajshekhar-kanthe <rajshekhar-kanthe@parallel-minds.com>
2025-09-07 16:26:11 +0530

    add a loop

--- helper_function_new.py
+++ helper_function_new.py
@@ -9,10 +9,26 @@ genai.configure(api_key="AIzaSyC8zxhvyNHtN1Qw-qHpUomA2Ct0auKN3sE")
 def ask_a_question(question, model="gemini-2.5-flash"):
     # Create model instance
     model = genai.GenerativeModel(model)
+
+
     
     # Generate content
     response = model.generate_content(question)
     return response.text
 
-# Example usage
+country_name = 'france'
 print(ask_a_question("What is the capital of France?"))
+print(ask_a_question(f"what is the capital of {country_name}?"))
+
+
+country_list = ["india", "japan","north korea"]
+
+for country in country_list :
+    print(ask_a_question(f"what is the capital of {country}"))
+    git 
+
+
+ 
+
+
+
