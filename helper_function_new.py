cc38189b17c73f14ab487cea08faff2bd77a6735
rajshekhar-kanthe <rajshekhar-kanthe@parallel-minds.com>
2025-09-08 06:17:14 +0530

    added more loop - lists

--- helper_function_new.py
+++ helper_function_new.py
@@ -23,8 +23,32 @@ print(ask_a_question(f"what is the capital of {country_name}?"))
 
 country_list = ["india", "japan","north korea"]
 
-for country in country_list :
-    print(ask_a_question(f"what is the capital of {country}"))
+#for country in country_list :
+#    print(ask_a_question(f"what is the capital of {country}"))
+country_list.append("thailand")
+print(country_list)
+country_list.remove("japan")
+print(country_list)
+for c_name in country_list:
+    print(c_name)
+ice_cream_flavours = ["Vanilla", "Chocolate", "Strawberry", "Mint"]
+flavour_desc = []
+i = 0
+for flavour in ice_cream_flavours:
+    # print(ask_a_question(f"""For the icecream flavour listed below
+     #              provide a captivating desecription that would be used for promotion
+     #              Flavor : {flavour}"""))
+    print(flavour)
+    question = f"""For the flavour {flavour},
+    provide a 10 words description"""
+    print(question)
+    # print(ask_a_question(question))
+    flavour_desc.append(ask_a_question(question))
+    print(flavour_desc[i])
+    i=i+1
+    
+print("done your task")
+
 
 
  
