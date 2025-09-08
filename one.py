ceefd8454e0cbb204880581d58b57c1237a5f87f
rajshekhar-kanthe <rajshekhar-kanthe@parallel-minds.com>
2025-09-08 09:32:46 +0530

    no changes

---
+++ one.py
@@ -0,0 +1,20 @@
+import helper_functions import print_llm_response
+
+
+print("Hello, World!")
+mylist =("one", 2, 3, False)
+print(mylist[0])
+print("""Hello
+      world
+      agai
+      """)
+print(type("hello"))
+
+print(75-32*5/9)
+print((75-32)*5/9)
+temp = (75-32)*5/9
+print(f"the temperature is {temp} degrees")
+print("the temperature is {temp} degrees")
+print_llm_response("what is the temperature in France?")
+
+
